3bac3dfaf1f5287964485b5f5dcec39dce2ae17d
Moisés <daw.moisesguerola@gmail.com>
2024-05-26 18:26:53 +0200

    Face Detector With YOLOv8 (need test)

--- backend/face_detector/src/models/FaceDetector.py
+++ backend/face_detector/src/models/FaceDetector.py
@@ -1,5 +1,43 @@
+from ultralytics import YOLO
+import numpy as np
+from io import BytesIO
+import cv2
+
+# FIXME: Type all methods
 class FaceDetector():
     def __init__(self) -> None:
-        pass
+        self.model = YOLO("../data/YOLOv8_face_detection.pt", task="detect")
     
-    # FIXME: init YOLOv8-face-detector model
+    def identify_faces(self, img_buffer: BytesIO):
+        img = cv2.imdecode(np.frombuffer(img_buffer.read(), np.uint8), 1)
+        results = self.model(img)
+        return results
+    
+    def get_boxes(self, results):
+        return results[0].boxes
+    
+    def pixelate(self, img, pixel_size = (20, 20)):
+        height, width = img.shape[:2]
+        small_image = cv2.resize(img, (width // pixel_size, height // pixel_size))
+        pixelated_image = cv2.resize(small_image, (width, height), interpolation=cv2.INTER_NEAREST)
+        
+        return pixelated_image
+    
+    def pixelate_faces(self, img: BytesIO):
+        results = self.identify_faces(img_buffer=img)
+        boxes = self.get_boxes(results)
+        
+        # NOTE: test this
+        _img = cv2.imread(img)
+        
+        for box in boxes:
+            x_min, y_min, x_max, y_max = map(int, box.xyxy.tolist()[0])
+            
+            face = _img[y_min:y_max, x_min:x_max]
+            blur_face = self.pixelate(_img)
+            
+            _img[y_min:y_max, x_min:x_max] = blur_face
+            
+            cv2.imwrite("./blur_image.png", _img)
+            
+            return _img
